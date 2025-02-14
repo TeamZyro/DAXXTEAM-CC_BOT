cdb91fbaa6670fa751c97ac7e4f018aae416583f
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-14 13:50:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ async def main():
   user = Client("6576720076",
                 api_id="27649783",
                 api_hash="834fd6015b50b781e0f8a41876ca95c8")
-  bot = Client("6576720076:AAFknz9qiSwRWcU4WXYwsx58Gp5JJqLJ3CA",
+  bot = Client("8009904311:AAHal98ZGIf_bVc5jusYx9Y0pbNdozO25Sk",
                api_id="27649783",
                api_hash="834fd6015b50b781e0f8a41876ca95c8",
-               bot_token="6576720076:AAFknz9qiSwRWcU4WXYwsx58Gp5JJqLJ3CA",
+               bot_token="8009904311:AAHal98ZGIf_bVc5jusYx9Y0pbNdozO25Sk",
                plugins=plugins)
   clients = [user, bot]
   bot.set_parse_mode(enums.ParseMode.HTML)
